569bee2d30bbb1110e88a9cdd9e44980215074c8
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-05 15:43:00 +0100

    creating an ErrorMessage component

--- src/App.js
+++ src/App.js
@@ -111,6 +111,10 @@ function Loader() {
   return <p className="loader">Loading...</p>;
 }
 
+function ErrorMessage({ message }) {
+  return <p className="error">{message}</p>;
+}
+
 function NavBar({ children }) {
   return (
     <nav className="nav-bar">

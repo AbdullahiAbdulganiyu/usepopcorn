730ea89ade67a6b6dc0e7bef72b05679bdb80c5f
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-05 14:00:01 +0100

    rendering the App component

--- src/index.js
+++ src/index.js
@@ -17,11 +17,11 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );

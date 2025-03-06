9f71eae83bb8f2a6863d7821717a965cf93b0681
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-06 14:46:14 +0100

    creating a function that handles movie selection

--- src/App.js
+++ src/App.js
@@ -59,6 +59,10 @@ export default function App() {
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
 
+  function handleSelectMovie(id) {
+    setSelectedId(id);
+  }
+
   useEffect(
     function () {
       async function fetchMovie() {

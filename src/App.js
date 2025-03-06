8531282e81c1d47cde3f4f1851e3044bf5b1aace
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-06 15:39:30 +0100

    rendering some details of the fetched movie on the UI

--- src/App.js
+++ src/App.js
@@ -282,9 +282,31 @@ function MovieDetails({ selectedId, onCloseMovie }) {
   }, []);
   return (
     <div className="details">
-      <button className="btn-back" onClick={onCloseMovie}>
-        &larr;
-      </button>
+      <header>
+        <button className="btn-back" onClick={onCloseMovie}>
+          &larr;
+        </button>
+        <img src={poster} alt={`Poster of ${movie} movie`} />
+        <div className="details-overview">
+          <h2>{title}</h2>
+          <p>
+            {released} &bull; {runtime}
+          </p>
+          <p>{genre}</p>
+          <p>
+            <span>⭐</span>
+            {imdbRating} IMDB rating
+          </p>
+        </div>
+      </header>
+
+      <section>
+        <p>
+          <em>{plot}</em>
+        </p>
+        <p>Staring {actor}</p>
+        <p>Directed by {director}</p>
+      </section>
       {selectedId}
     </div>
   );

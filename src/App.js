7cf84bc429c11733c981201af62414a49dc95e74
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-11 14:34:44 +0100

    initializing the watched state with the movie in the localStorage

--- src/App.js
+++ src/App.js
@@ -8,10 +8,15 @@ const KEY = "7fa2bfb2";
 export default function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
+  // const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem("watched");
+    return JSON.parse(storedValue);
+  });
 
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));

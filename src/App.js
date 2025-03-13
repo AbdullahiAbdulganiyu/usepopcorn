9144b9f39a70646dc09eb9ce6ad97456f8807a2d
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-13 10:43:43 +0100

    extracting some code from app.js to the custom hook(useMovie)

--- src/App.js
+++ src/App.js
@@ -43,47 +43,6 @@ export default function App() {
     },
     [watched]
   );
-  useEffect(
-    function () {
-      const controller = new AbortController();
-      async function fetchMovie() {
-        try {
-          setIsLoading(true);
-          setError("");
-          const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-            { signal: controller.signal }
-          );
-          if (!res.ok)
-            throw new Error("Something went wrong while fetching the movie");
-          const data = await res.json();
-          if (data.Response === "False") throw new Error("Movie Not found");
-          setMovies(data.Search);
-          setError("");
-        } catch (err) {
-          if (err.name !== "AbortError") {
-            console.log(err.message);
-            setError(err.message);
-          }
-        } finally {
-          setIsLoading(false);
-        }
-      }
-
-      if (query.length < 3) {
-        setMovies([]);
-        setError("");
-        return;
-      }
-
-      handleCloseMovie();
-      fetchMovie();
-      return function () {
-        controller.abort();
-      };
-    },
-    [query]
-  );
   return (
     <>
       <NavBar>

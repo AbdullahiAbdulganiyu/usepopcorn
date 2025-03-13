0af4bede58fe30ee4577c0ccedd214776b15c834
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-13 21:35:45 +0100

    creating the watched state

--- src/useLocalStorageState.js
+++ src/useLocalStorageState.js
@@ -1,1 +1,6 @@
-export function useLocalStorageState() {}
+export function useLocalStorageState() {
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem("watched");
+    return JSON.parse(storedValue);
+  });
+}

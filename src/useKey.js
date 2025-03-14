7ef3c3d064a5a8f0bd312d727dccc09398fdecec
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-14 09:41:44 +0100

    exporting it as a function

--- src/useKey.js
+++ src/useKey.js
@@ -1,17 +1,19 @@
 import { useEffect } from "react";
 
-useEffect(
-  function () {
-    function callBack(e) {
-      if (e.code === "Escape") {
-        onCloseMovie();
+export function useKey() {
+  useEffect(
+    function () {
+      function callBack(e) {
+        if (e.code === "Escape") {
+          onCloseMovie();
+        }
       }
-    }
-    document.addEventListener("keydown", callBack);
+      document.addEventListener("keydown", callBack);
 
-    return function () {
-      document.removeEventListener("keydown", callBack);
-    };
-  },
-  [onCloseMovie]
-);
+      return function () {
+        document.removeEventListener("keydown", callBack);
+      };
+    },
+    [onCloseMovie]
+  );
+}

e5c148dca01683710c50f0a295738512c6911ee5
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-14 09:55:38 +0100

    commenting out replaced code

--- src/App.js
+++ src/App.js
@@ -135,22 +135,22 @@ function Search({ query, setQuery }) {
   });
 
   const inputEl = useRef(null);
-  useEffect(
-    function () {
-      function callback(e) {
-        if (document.activeElement === inputEl.current) return;
-        if (e.code === "Enter") {
-          inputEl.current.focus();
-          setQuery("");
-        }
-      }
+  // useEffect(
+  //   function () {
+  //     function callback(e) {
+  //       if (document.activeElement === inputEl.current) return;
+  //       if (e.code === "Enter") {
+  //         inputEl.current.focus();
+  //         setQuery("");
+  //       }
+  //     }
 
-      document.addEventListener("keydown", callback);
+  //     document.addEventListener("keydown", callback);
 
-      return () => document.addEventListener("keydown", callback);
-    },
-    [setQuery]
-  );
+  //     return () => document.addEventListener("keydown", callback);
+  //   },
+  //   [setQuery]
+  // );
   return (
     <input
       className="search"
